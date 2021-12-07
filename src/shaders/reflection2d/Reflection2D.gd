707f489b73b3124066a137a9bc1546bcafdeb0e9
Steven <stevenrogge95@gmail.com>
2021-12-07 03:21:41 +0100

    watershaper prototype

--- src/shaders/reflection2d/Reflection2D.gd
+++ src/shaders/reflection2d/Reflection2D.gd
@@ -5,9 +5,17 @@ extends Sprite
 
 func _ready() -> void:
 	connect("item_rect_changed", self, "_on_item_rect_changed")
+	connect("texture_changed", self, "update_aspect_ratio")
+	update_aspect_ratio()
 
 
-func _process(delta: float) -> void:
+func update_aspect_ratio() -> void:
+	#calculating texture size and ratio before passing it to the shader
+	var size: Vector2 = texture.get_size()
+	material.set_shader_param("aspect_ratio", size.x / size.y)
+	
+
+func _process(_delta: float) -> void:
 	update_zoom(get_viewport_transform().y.y)
 
 
